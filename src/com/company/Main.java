b2d332ad1111c0b6c07384f21e22fbb57e17853a
bryce_000 <bs482@pvsd.org>
2018-11-08 20:07:18 -0500

    finished student class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,7 @@ import java.util.*;
 public class Main
 {
     public static void main(String[] args)
-    throws IOException,NoSuchElementException
-
+    throws IOException
     {
         Scanner sf = new Scanner(new File("C:\\Users\\bryce_000\\IdeaProjects\\twp-read-this-2018-bps24\\src\\Imports.txt"));
         int maxIndx = -1;
@@ -14,8 +13,11 @@ public class Main
         Student [] a = new Student[50];
         String fir=null;
         String las=null;
+        String ave1=null;
+        String abs1=null;
         int ave=0;
         int abs=0;
+        String control=1;
 
         while(sf.hasNext( ))
         {
@@ -25,17 +27,38 @@ public class Main
 
         sf.close( );
 
-        for (int i=0;i<=maxIndx;i++)
+        for (int i=0;i<maxIndx;i++)
         {
-           Scanner sf2 = new Scanner(text[i+1]).useDelimiter("\\s*");
-           fir=sf2.next();
-           //las=sf2.next();
-           //ave=sf2.nextInt();
-           //abs=sf2.nextInt();
+           Scanner sf2 = new Scanner(text[i+1]);
+           sf2.useDelimiter("\t");
 
-           a[i]=new Student(fir,"as",1,2);
+            fir=sf2.next();
+            las=sf2.next();
+            ave1=sf2.next();
+            abs1=sf2.next();
+
+            ave1=ave1.substring(0,ave1.length()-1);
+
+            ave=Integer.valueOf(ave1);
+            abs=Integer.valueOf(abs1);
+
+           a[i]=new Student(fir,las,ave,abs);
         }
 
-        System.out.println(a[4].getFirst());
+        System.out.println("Class Average:" + Student.classAve);
+        System.out.println("Number of A's: " + Student.as);
+        System.out.println("Number of B's: " + Student.bs);
+        System.out.println("Number of C's: " + Student.cs);
+        System.out.println("Number of D's: " + Student.ds);
+        System.out.println("Number of F's: " + Student.fs);
+
+        while(control!="0")
+        {
+            Scanner kb= new Scanner(System.in);
+            System.out.println("Would you like to search for a student? ");
+            System.out.println("Enter a student's first name or enter 0 to exit");
+            kb.next()=control;
+            
+        }
     }
 }
--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -6,6 +6,14 @@ public class Student
     private String last;
     private int average;
     private int ab;
+    public static double classAve;
+    public int numofstudents;
+    public double classTot=0;
+    public static int as;
+    public static int bs;
+    public static int cs;
+    public static int ds;
+    public static int fs;
 
     public Student(String first, String last, int average, int ab)
     {
@@ -13,25 +21,24 @@ public class Student
         this.last=last;
         this.average=average;
         this.ab=ab;
+
+        classTot += average;
+        numofstudents ++;
+        classAve=classTot/numofstudents;
+
+        if(average>=90)  as++;
+        else if(average>=80) bs++;
+        else if(average>=70)  cs++;
+        else if(average>=60) ds++;
+        else fs++;
     }
 
-    public String getFirst()
-    {
-        return first;
-    }
+    public String getFirst(){ return first; }
 
-    public String getLast(Student a)
-    {
-        return a.last;
-    }
+    public String getLast() { return last; }
 
-    public int getAverage(Student a)
-    {
-        return a.average;
-    }
+    public int getAverage() { return average; }
 
-    public int getAb(Student a)
-    {
-        return a.ab;
-    }
+    public int getAb() { return ab; }
+
 }
